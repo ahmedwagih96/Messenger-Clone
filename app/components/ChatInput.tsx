c87ec2301c36c6af788802b3ccad2d4255ebd48d
Ahmed Wagih <ahmd.wagih96@gmail.com>
2022-11-28 15:26:03 +0200

    created the chatInput component

--- app/components/ChatInput.tsx
+++ app/components/ChatInput.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { FormEvent, useState } from "react";
+
+type Props = {};
 
 function ChatInput({}: Props) {
+  const [input, setInput] = useState<string>("");
+  const addMessage = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!input) return;
+    const messageToSend = input;
+    setInput("");
+  };
   return (
-    <div>ChatInput</div>
-  )
+    <form
+      onSubmit={addMessage}
+      className="flex gap-2 px-10 py-2 border-t border-gray-100 fixed bottom-0 z-50 w-full"
+    >
+      <input
+        type="text"
+        className="flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed"
+        placeholder="Enter message here.."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+        disabled={!input}
+      >
+        Send
+      </button>
+    </form>
+  );
 }
 
-export default ChatInput
+export default ChatInput;
